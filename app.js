93ba20350fe0c3bd22abc7884696ec9b92db4c32
Aspen Ishii <ishii.aspen@gmail.com>
2024-05-24 10:07:05 -0600

    game board color and set up

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const gameBoard = document.querySelector("#gameboard");
 const playerDisplay = document.querySelector("#player");
-document.querySelector("#info-display");
+const infoDisplay = document.querySelector("#info-display");
 
 /// Array of pieces
-const width = 8
+const width = 8;
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
@@ -13,16 +13,35 @@ const startPieces = [
     '', '', '', '', '', '', '', '',
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
     rook, knight, bishop, queen, king, bishop, knight, rook,
-]
+];
 
 function createBoard() {
-    startPieces.forEach((startPieces, i) => {
-        const square = document.createElement('div')
-        square.classList.add('square')
-        square.innerHTML = startPieces
-        square.setAttribute('square-id', i)
-        square.classList.add('beige')
-        gameBoard.append(square)
+    startPieces.forEach((piece, i) => {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.innerHTML = piece;
+        square.setAttribute('square-id', i);
+        //square.classList.add('beige')
+        //Alt tile color
+        const row = Math.floor ((63 - i) / 8) + 1
+        if (row % 2 === 0) {
+            square.classList.add(i % 2 === 0 ? "beige" : "brown");
+        } else {
+            square.classList.add(i % 2 === 0 ?  "brown" : "beige");
+        }
+
+        if (piece !== '') {
+            const pieceElement = square.querySelector('svg');
+            if (pieceElement) {
+                if (i <= 15) {
+                    pieceElement.classList.add("black");
+                } else if (i >= 48) {
+                    pieceElement.classList.add("white");
+                }
+            }
+        }
+        gameBoard.append(square);
     })
 }
-createBoard()
+
+createBoard();
